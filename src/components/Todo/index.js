7fe379794906fba8cfb0ac93ccbb1d8cd21a5d44
Doomitrii <podorvanov-dmitrii@yandex.ru>
2022-11-10 20:27:46 +0500

    fix - Todo const Todo and added value check

--- src/components/Todo/index.js
+++ src/components/Todo/index.js
@@ -6,15 +6,19 @@ import all from '../../assets/icons/all.svg'
 import DB from '../../assets/DB.json'
 
 const Todo = () => {
-    const [lists, setList] = React.useState(DB.lists.map(item=>
-        {
-            item.color = DB.colors.find (
-                // color => color.id === colorId).name
-                color => color.id === item.id).name;
+    const [lists, setList] = React.useState(DB.lists.map(item => {
+            let color = DB.colors.find(
+                color => color.id === item.colorId);
+            if (color != null) {
+                item.color = color.name
+            }
+            else {
+                item.color= "black"
+            }
             return item;
         }
     ))
-    const onAddList=(obj)=> {
+    const onAddList = (obj) => {
         const newList = [
             ...lists, obj
         ];
@@ -25,23 +29,23 @@ const Todo = () => {
         <div className='todo'>
             <div className="todo__sideBar">
                 <List
-                items = {[
-                    {
-                        icon: all,
-                        name: "Вcе задачи!",
-                        active: true
-                    }
-                ]}
+                    items={[
+                        {
+                            icon: all,
+                            name: "Вcе задачи!",
+                            active: true
+                        }
+                    ]}
                 />
-                <List items = {lists}
-                    isRemovable
+                <List items={lists}
+                      isRemovable
                 />
-                <Popup onAdd={onAddList} colors = {DB.colors} />
+                <Popup onAdd={onAddList} colors={DB.colors}/>
 
             </div>
 
             <div className="todo__tasks">
-            <span>123465789</span>
+                <span>123465789</span>
             </div>
         </div>
     )
